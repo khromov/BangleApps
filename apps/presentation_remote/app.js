38dcb75a650af4adbb2ecf49a179c0471bf8543e
Stanislav Khromov <stanislav+github@khromov.se>
2022-08-28 22:18:50 +0200

    Use ble_hid_combo with tapKey and add error logging

--- apps/presentation_remote/app.js
+++ apps/presentation_remote/app.js
@@ -1,77 +1,56 @@
 const storage = require('Storage');
 const settings = storage.readJSON('setting.json', 1) || { };
-
-var sendHid, next, prev, toggle, up, down; //, profile;
-var lasty = 0;
-var lastx = 0;
+const logger = require("Storage").open("error-log.csv", "a");
 
 var usbHidEnabled = false;
 
 let currentlySendingKey = false;
 
+var forward, backward;
+
 // Power savings stuff
 Bangle.setLocked(false);
-// Bangle.setLCDTimeout(5);
 Bangle.setOptions({ lockTimeout: 0 });
-// Bangle.setLCDPower(1);
 
-console.log(settings.HID);
 if (settings.HID=="kb" || settings.HID=="kbmedia") {
   usbHidEnabled = true;
-  
+
   // https://www.espruino.com/BLE+Keyboard + presentor app
   // https://www.espruino.com/modules/USBKeyboard.js
-  const kb = require("ble_hid_keyboard");
+  const kb = require("ble_hid_combo");
   NRF.setServices(undefined, { hid : kb.report });
 
   // http://forum.espruino.com/conversations/325524/
-  NRF.setConnectionInterval(100); // Test to see if it reduces keystroke ghosting
+  // NRF.setConnectionInterval(100);
 
-  // OLD?
-  // https://gist.github.com/MightyPork/6da26e382a7ad91b5496ee55fdc73db2
-  sendHid = function (code, cb) {
-    try {
-      return new Promise( resolve => {
-        NRF.sendHIDReport([2,0,0,code,0,0,0,0,0], () => {
-          NRF.sendHIDReport([2,0,0,0,0,0,0,0,0], resolve);
-        });
-
-        console.log(cb);
-        if (cb) {
-           cb();
-        }
-      });
-    } catch(e) {
-      // TODO: Error log here
-      print(e);
-    }
-  };
   forward = function (cb) { 
-    // sendHid(0x4f, cb);
     if(!currentlySendingKey) {
       currentlySendingKey = true;
       try {
-        kb.tap(kb.KEY.RIGHT, 0, function(data) {
+        kb.tapKey(kb.KEY.RIGHT, 0, function(data) {
           currentlySendingKey = false;
-          console.log("Sent!", data);
+          // console.log("Sent forward!", data);
+          logger.write("Sent forward!"+"\n");
         });
       } catch(e) {
         currentlySendingKey = false;
+        logger.write(e +"\n");
         console.log("Could not send forward event", e);
       }
     }
   };
   backward = function (cb) {
-    //sendHid(0x50, cb);
     if(!currentlySendingKey) {
       currentlySendingKey = true;
       try {
-        kb.tap(kb.KEY.LEFT, 0, function(data) {
+        kb.tapKey(kb.KEY.LEFT, 0, function(data) {
           currentlySendingKey = false;
-          console.log("Sent!", data);
+          // console.log("Sent backward!", data);
+          logger.write("Sent backward!"+"\n");
         });
       } catch(e) {
         currentlySendingKey = false;
+        logger.write(e +"\n");
         console.log("Could not send backward event", e);
       }
     }
@@ -102,7 +81,7 @@ if (usbHidEnabled) {
   setWatch(function(data) {
     console.log("> Go forward", data);
      forward();
-  }, BTN1, { edge: "rising", repeat: true, debounce: 50 });
+  }, BTN1, { edge: "rising", repeat: true, debounce: 100 });
 
   Bangle.on('touch', (button, data) => { 
     console.log("< Go back", button, data);
